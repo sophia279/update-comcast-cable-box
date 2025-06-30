f1c07175256d7db21162875346899e363544cd43
sophia279 <sophiadavis1921@outlook.com>
2025-06-30 18:00:48 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'Comcast Guide'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+html_js_files = [
+    'chat.js',]
+
+html_favicon = '_static/comcast.png'
